b7ed8c13d2995653de4c5d4a166b2c8ec724e948
NhanBui0204 <nhan.bui1@rikai.technology>
2024-12-04 09:48:12 +0700

    fix_api_get_FE

--- server/server/urls.py
+++ server/server/urls.py
@@ -39,7 +39,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
+    path("api/", include("users.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
--- server/users/urls.py
+++ server/users/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('categories/<int:pk>/', views.category_detail, name='category-detail'),
     path('subcategories/', views.subcategory_view, name='subcategory-list'),
     path('subcategories/<int:pk>/', views.subcategory_detail, name='subcategory-detail'),
-    path('articles/<int:pk>/', views.article_detail, name='article-detail'),  
-    path('articles/', views.article_create_by_subcategory, name='article-create-by-subcategory'),  
-    path('articles/all/', views.article_get_all, name='article_get_all'),
-    path('comments/article/<int:article_id>/', views.add_comment, name='add-comment'),
+    path('articles/<int:pk>/', views.article_detail, name='articles-detail'),  
+    path('articles/', views.article_create_by_subcategory, name='articles-create-by-subcategory'),  
+    path('articles/all/', views.article_get_all, name='articles_get_all'),
+    path('comments/articles/<int:articles_id>/', views.add_comment, name='add-comment'),
     path('comments/<int:id>/', views.comment_detail, name='comment_detail'),
 ]
